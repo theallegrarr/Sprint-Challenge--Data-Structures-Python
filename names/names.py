2f0fa80d37bd253fa53657cc1e9a3bd7d26cc1b5
theallegrarr <nurudeen.amedu@gmail.com>
2020-02-28 11:21:00 +0100

    variable names updated in names.py

--- names/names.py
+++ names/names.py
@@ -12,23 +12,22 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
-duplicates = []
-bst = BinarySearchTree('duplicate_names')
-
-# insert the names into the tree
-for name in names_1:
-    bst.insert(name)
-# check if names in names 2 are also inside names 1
-for name in names_2:
-    # if it is there, insert into the duplicates
-    if bst.contains(name):
-        duplicates.append(name)
+binary_search = BinarySearchTree('duplicate_names')
 
 # Replace the nested for loops below with your improvements
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
+
+# insert the names into the tree
+for name in names_1:
+    binary_search.insert(name)
+# check if names in names 2 are also inside names 1
+for name in names_2:
+    # if it is there, insert into the duplicates
+    if binary_search.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
